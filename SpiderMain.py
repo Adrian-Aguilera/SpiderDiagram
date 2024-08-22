1f3f04a332d30ab85a4e1f5d2d23e9dd892c2b83
Adrian-Aguilera <cruz74270@gmail.com>
2024-08-21 22:04:35 -0600

    fix: download img

--- SpiderMain.py
+++ SpiderMain.py
@@ -1,20 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL
-url = 'https://www.superselectos.com/'
-response = requests.get(url)
+def downloadIMG(url):
+    r = requests.get(url).content
+    with open('img.jpg', 'wb+') as i:
+        i.write(r)
 
-#parcer el contenido de la pagina a texto
-soup = BeautifulSoup(response.content, 'html.parser')
 
-links = soup.find_all('a')
-print("\nEnlaces:")
-for link in links:
-    print(link.get('href'))
+if __name__ == "__main__":
+    url = 'https://www.superselectos.com/'
+    response = requests.get(url)
 
-# Extraer y mostrar todas las imagenes
-images = soup.find_all('img')
-print("\nImágenes:")
-for img in images:
-    print(img.get('src'))
+    #parcer el contenido de la pagina a texto
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    links = soup.find_all('a')
+    print("\nEnlaces:")
+    for link in links:
+        print(link.get('href'))
+
+    # Extraer y mostrar todas las imagenes
+    images = soup.find_all('img')
+    print("\nImágenes:")
+    for img in images:
+        imgUrl = img.get('src')
+        downloadIMG(imgUrl)
+
